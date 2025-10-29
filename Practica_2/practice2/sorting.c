32b70e22272faa2ca8719bc51285272cf5e81e21
Daniel-Martz <danieluniversidad006@gmail.com>
2025-10-29 18:17:55 +0100

    ss

--- Practica_2/practice2/sorting.c
+++ Practica_2/practice2/sorting.c
@@ -9,30 +9,164 @@
  *
  */
 
-
 #include "sorting.h"
+#include <stdio.h>
+
+int partition(int *tabla, int ip, int iu, int *pos)
+{
+  int m;
+  int k, aux, i;
+
+  m = median(tabla, ip, iu, pos);
+  k = tabla[m];
+  aux = tabla[m];
+  tabla[m] = tabla[ip];
+  tabla[ip] = tabla[aux];
+  m = ip;
+  for (i = ip + 1; i <= iu; i++)
+  {
+    if (tabla[i] < k)
+    {
+      m++;
+      aux = tabla[m];
+      tabla[m] = tabla[i];
+      tabla[i] = tabla[aux];
+    }
+    aux = tabla[m];
+    tabla[m] = tabla[ip];
+    tabla[ip] = tabla[aux];
+  }
+  return m;
+}
+
+int median(int *tabla, int ip, int iu, int *pos)
+{
+  return ip;
+}
 
 /***************************************************/
 /* Function: InsertSort    Date:                   */
 /* Your comment                                    */
 /***************************************************/
-int InsertSort(int* array, int ip, int iu)
+int InsertSort(int *array, int ip, int iu)
 {
-  /* Your code */
+  int i, j, OB = 0, aux;
+
+  if (array == NULL)
+  {
+    fprintf(stderr, "El array es invalido.\n");
+    return ERR;
+  }
+  if (ip < 0 || iu < 0)
+  {
+    fprintf(stderr, "Los indices tienen que ser positivos.");
+    return ERR;
+  }
+  if (ip > iu)
+  {
+    fprintf(stderr, "El primer indice tiene que ser mayor que el ultimo.\n");
+    return ERR;
+  }
+
+  for (i = ip + 1; i <= iu; i++)
+  {
+    aux = array[i];
+    j = i - 1;
+    while (j >= ip && array[j] > aux)
+    {
+      array[j + 1] = array[j];
+      OB++;
+      j--;
+    }
+    if (j >= ip)
+      OB++;
+    array[j + 1] = aux;
+  }
+
+  return OB;
 }
 
-
 /***************************************************/
-/* Function: SelectSort    Date:                   */
+/* Function: SelectSort    Date: 25/09/2025        */
 /* Your comment                                    */
 /***************************************************/
-int BubbleSort(int* array, int ip, int iu)
+int BubbleSort(int *array, int ip, int iu)
 {
-  /* Your code */
+  int flag = 1;
+  int i = iu;
+  int j = 0;
+  int aux = 0;
+  int OB = 0;
+
+  if (array == NULL)
+  {
+    fprintf(stderr, "El array es invalido.\n");
+    return ERR;
+  }
+  if (ip < 0 || iu < 0)
+  {
+    fprintf(stderr, "Los indices tienen que ser positivos.");
+    return ERR;
+  }
+  if (ip > iu)
+  {
+    fprintf(stderr, "El primer indice tiene que ser mayor que el ultimo.\n");
+    return ERR;
+  }
+
+  while ((flag == 1) && (i > ip))
+  {
+    flag = 0;
+    for (j = ip; j < i; j++)
+    {
+      if (array[j] > array[j + 1])
+      {
+        aux = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = aux;
+        flag = 1;
+      }
+      OB++;
+    }
+    i--;
+  }
+  return OB;
 }
 
+/***************************************************/
+/* Function: quicksort    Date: 23/10/2025         */
+/* Your comment                                    */
+/***************************************************/
+int BubbleSort(int *array, int ip, int iu)
+{
+}
 
+/***************************************************/
+/* Function: mergesort    Date: 23/10/2025         */
+/* Your comment                                    */
+/***************************************************/
 
+int mergesort(int *array, int ip, int iu)
+{
+  int p_medio;
 
+  if ((array == NULL) || (iu < ip))
+  {
+    return ERR;
+  }
+  if (ip == iu)
+  {
+    reutnr OK;
+  }
+  p_medio = (ip + iu) / 2;
+  
+  if (mergesort(array, ip, p_medio) == ERR)
+    return ERR;
 
+  if (mergesort(array, p_medio + 1, iu) == ERR)
+    return ERR;
+  
+  merge(array, ip, p_medio, iu);
+}
 
+int merge(int *tabla, int ip, int iu, int imedio);
--- Practica_2/practice2/times.c
+++ Practica_2/practice2/times.c
@@ -11,18 +11,73 @@
 
 #include "times.h"
 #include "sorting.h"
+#include "permutations.h"
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 /***************************************************/
 /* Function: average_sorting_time Date:            */
 /*                                                 */
 /* Your documentation                              */
 /***************************************************/
-short average_sorting_time(pfunc_sort metodo, 
-                              int n_perms,
-                              int N, 
-                              PTIME_AA ptime)
+short average_sorting_time(pfunc_sort metodo,
+                           int n_perms,
+                           int N,
+                           PTIME_AA ptime)
 {
-/* Your code */
+  int i, max_OB = -1, min_OB = -1, OB_aux, **array_perms;
+  double sum_OB = 0;
+  clock_t start, end;
+
+  if (metodo == NULL || ptime == NULL || n_perms <= 0 || N <= 0)
+  {
+    fprintf(stderr, "Las variables introducidas no son válidas.");
+    return ERR;
+  }
+
+  ptime->n_elems = n_perms;
+  ptime->N = N;
+
+  array_perms = generate_permutations(n_perms, N);
+  if (array_perms == NULL)
+  {
+    return ERR;
+  }
+  start = clock();
+  for (i = 0; i < n_perms; i++)
+  {
+
+    OB_aux = metodo(array_perms[i], 0, N - 1);
+
+    if (max_OB == -1)
+    {
+      max_OB = OB_aux;
+    }
+    if (min_OB == -1)
+    {
+      min_OB = OB_aux;
+    }
+
+    if (OB_aux > max_OB)
+    {
+      max_OB = OB_aux;
+    }
+    if (OB_aux < min_OB)
+    {
+      min_OB = OB_aux;
+    }
+    sum_OB += OB_aux;
+
+    free(array_perms[i]);
+  }
+  end = clock();
+  ptime->average_ob = sum_OB / n_perms;
+  ptime->max_ob = max_OB;
+  ptime->min_ob = min_OB;
+  ptime->time = (double)(end - start) / CLOCKS_PER_SEC;
+  free(array_perms);
+  return OK;
 }
 
 /***************************************************/
@@ -30,11 +85,43 @@ short average_sorting_time(pfunc_sort metodo,
 /*                                                 */
 /* Your documentation                              */
 /***************************************************/
-short generate_sorting_times(pfunc_sort method, char* file, 
-                                int num_min, int num_max, 
-                                int incr, int n_perms)
+short generate_sorting_times(pfunc_sort method, char *file,
+                             int num_min, int num_max,
+                             int incr, int n_perms)
 {
-  /* Your code */
+  int i, n_times;
+  PTIME_AA ptime = NULL;
+
+  if (!method || num_min <= 0 || num_max < num_min || incr <= 0)
+  {
+    fprintf(stderr, "Las variables introducidas no son válidas.\n");
+    return ERR;
+  }
+
+  n_times = ((num_max - num_min) / incr) + 1;
+
+  if (!(ptime = (PTIME_AA)calloc(n_times, sizeof(TIME_AA))))
+  {
+    fprintf(stderr, "Error al reservar memoria para la estructura del tiempo.\n");
+    return ERR;
+  }
+  for (i = 0; i < n_times; i++)
+  {
+    int N = num_min + i * incr;
+    if (average_sorting_time(method, n_perms, N, &ptime[i]) == ERR)
+    {
+      free(ptime);
+      return ERR;
+    }
+  }
+  if (save_time_table(file, ptime, n_times) == ERR)
+  {
+    free(ptime);
+    return ERR;
+  }
+
+  free(ptime);
+  return OK;
 }
 
 /***************************************************/
@@ -42,9 +129,35 @@ short generate_sorting_times(pfunc_sort method, char* file,
 /*                                                 */
 /* Your documentation                              */
 /***************************************************/
-short save_time_table(char* file, PTIME_AA ptime, int n_times)
+short save_time_table(char *file, PTIME_AA ptime, int n_times)
 {
-  /* your code */
+  FILE *fout = NULL;
+  int i;
+
+  if (!ptime || n_times <= 0 || !file)
+  {
+    fprintf(stderr, "Las variables introducidas no son válidas.");
+    return ERR;
+  }
+
+  if (!(fout = fopen(file, "w")))
+  {
+    fprintf(stderr, "Error al abrir el fichero");
+    return ERR;
+  }
+
+  fprintf(fout, "%-6s %-12s %-14s %-8s %-8s\n", "N", "Time", "Average_OB", "Max_OB", "Min_OB");
+
+  for (i = 0; i < n_times; i++)
+  {
+    fprintf(fout, "%-6i %-12.6f %-14.2f %-8i %-8i\n",
+            ptime[i].N,
+            ptime[i].time,
+            ptime[i].average_ob,
+            ptime[i].max_ob,
+            ptime[i].min_ob);
+  }
+
+  fclose(fout);
+  return OK;
 }
-
-
