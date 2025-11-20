a8d3efde6756b603ebba79090e8a8caddbf06fbb
Rodri0707 <rodru0707@gmail.com>
2025-11-20 18:22:07 +0100

    r

--- practica3/Pr3/search.c
+++ practica3/Pr3/search.c
@@ -11,18 +11,20 @@
 
 #include "search.h"
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 /**
  *  Key generation functions
  *
  *  Description: Receives the number of keys to generate in the n_keys
  *               parameter. The generated keys go from 1 to max. The
- * 				 keys are returned in the keys parameter which must be 
+ * 				 keys are returned in the keys parameter which must be
  *				 allocated externally to the function.
  */
-  
+
 /**
  *  Function: uniform_key_generator
  *               This function generates all keys from 1 to max in a sequential
@@ -32,7 +34,8 @@ void uniform_key_generator(int *keys, int n_keys, int max)
 {
   int i;
 
-  for(i = 0; i < n_keys; i++) keys[i] = 1 + (i % max);
+  for (i = 0; i < n_keys; i++)
+    keys[i] = 1 + (i % max);
 
   return;
 }
@@ -40,7 +43,7 @@ void uniform_key_generator(int *keys, int n_keys, int max)
 /**
  *  Function: potential_key_generator
  *               This function generates keys following an approximately
- *               potential distribution. The smaller values are much more 
+ *               potential distribution. The smaller values are much more
  *               likely than the bigger ones. Value 1 has a 50%
  *               probability, value 2 a 17%, value 3 the 9%, etc.
  */
@@ -48,54 +51,97 @@ void potential_key_generator(int *keys, int n_keys, int max)
 {
   int i;
 
-  for(i = 0; i < n_keys; i++) 
+  for (i = 0; i < n_keys; i++)
   {
-    keys[i] = .5+max/(1 + max*((double)rand()/(RAND_MAX)));
+    keys[i] = .5 + max / (1 + max * ((double)rand() / (RAND_MAX)));
   }
 
   return;
 }
 
-PDICT init_dictionary (int size, char order)
+PDICT init_dictionary(int size, char order)
 {
-	/* your code */
+  int i;
+  if (size <= 0)
+  {
+    return NULL;
+  }
+  PDICT dictionary = malloc(sizeof(DICT));
+  if (dictionary == NULL)
+  {
+    fprintf(stderr,"Error en la creacion del diccionario.");
+    return NULL;
+  }
+
+  dictionary->n_data = 0;
+  dictionary->size = size;
+  dictionary->order = order;
+  dictionary->table = (int *)calloc(size, sizeof(int));
+  if (dictionary->table == NULL)
+  {
+    fprintf(stderr,"Error en la creacion de la tabla del diccionario.");
+    free(dictionary);
+    return NULL;
+  }
+
+  for (i = 0; i < size; i++)
+  {
+    dictionary->table[i] = 0;
+  }
+
+  return dictionary;
 }
 
 void free_dictionary(PDICT pdict)
 {
-	/* your code */
+  if (pdict == NULL)
+  {
+    return;
+  }
+  free(pdict->table);
+  free(pdict);
+  return;
 }
 
 int insert_dictionary(PDICT pdict, int key)
 {
-	/* your code */
+
+  if (!pdict || pdict->size <= 0 || pdict->n_data < 0 || pdict->n_data >= pdict->size || !pdict->table || pdict->order != ){
+    fprintf(stderr,"Error en los parametros de entrada de insert_dictionary");
+    return ERR;
+  }
+    if (pdict->order == NOT_SORTED)
+    {
+      pdict->table[pdict->data] = key;
+      pdict->n_data++;
+    }
+    else
+    {
+    }
 }
 
-int massive_insertion_dictionary (PDICT pdict,int *keys, int n_keys)
+int massive_insertion_dictionary(PDICT pdict, int *keys, int n_keys)
 {
-	/* your code */
+  /* your code */
 }
 
 int search_dictionary(PDICT pdict, int key, int *ppos, pfunc_search method)
 {
-	/* your code */
+  /* your code */
 }
-
 
 /* Search functions of the Dictionary ADT */
-int bin_search(int *table,int F,int L,int key, int *ppos)
+int bin_search(int *table, int F, int L, int key, int *ppos)
 {
-	/* your code */
+  /* your code */
 }
 
-int lin_search(int *table,int F,int L,int key, int *ppos)
+int lin_search(int *table, int F, int L, int key, int *ppos)
 {
-	/* your code */
+  if ((table == NULL) || (F>L))
 }
 
-int lin_auto_search(int *table,int F,int L,int key, int *ppos)
+int lin_auto_search(int *table, int F, int L, int key, int *ppos)
 {
-	/* your code */
+  /* your code */
 }
-
-
